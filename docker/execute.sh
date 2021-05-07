4c67dd3c3c822e2fb30bdfd5e74e2d9d60e86f6f
danielmurteira <domurteira@gmail.com>
2021-05-07 18:06:18 +0100

    Docker script correction

--- docker/execute.sh
+++ docker/execute.sh
@@ -13,7 +13,6 @@ cp access2csv/target/${SPARK_APPLICATION_JAR_NAME}.jar ${SPARK_APPLICATION_JAR_L
 #sh /submit.sh
 
 /spark/bin/spark-submit --class com.ddom.access2csv.AccdbToCsv \
-    --master "yarn" --deploy-mode "cluster" \
     --jars "/ucanaccess-5.0.1.jar,/jackcess-3.0.1.jar,/hsqldb-2.5.0.jar" \
     --files "/file.accdb,/params.json" \
     "/access2csv-1.0.0.jar" "/params.json"
